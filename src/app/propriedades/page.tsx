7e4545f3f7b94f6ed5369a7f27cbd1d3884174d2
fdanctl <daniel.a.d.freire.00@gmail.com>
2025-01-15 23:57:49 +0000

    update

--- src/app/propriedades/page.tsx
+++ src/app/propriedades/page.tsx
@@ -2,28 +2,30 @@ import { Card } from "@/components/Card";
 import { ResponsePropriedadesDTO } from "@/dto/propriedades";
 import Link from "next/link";
 
-export async function Page() {
-  const res = await fetch("/api/propriedades");
-  const propriedades: ResponsePropriedadesDTO[] = await res.json();
-  return (
-    <>
-      <h1>Propriedades</h1>
-      <div className="grid grid-cols-3 gap-2">
-        {propriedades.map((e) => (
-          <Card
-            _id={e._id}
-            morada={e.morada}
-            localizacao={e.localizacao}
-            quartos={e.quartos}
-            mensalidade={e.mensalidade}
-            valorEmprestimo={e.valorEmprestimo}
-            rendimentos={e.rendimentos}
-            ganhosMensais={e.ganhosMensais}
-            mesesRestantes={e.mesesRestantes}
-          />
-        ))}
-      </div>
-      <Link href="/" className="text-2xl">+</Link>
-    </>
-  );
+export default async function Page() {
+    const res = await fetch("/api/propriedades");
+    const propriedades: ResponsePropriedadesDTO[] = await res.json();
+    return (
+        <>
+            <h1>Propriedades</h1>
+            <div className="grid grid-cols-3 gap-2">
+                {propriedades.map((e) => (
+                    <Card
+                        _id={e._id}
+                        morada={e.morada}
+                        localizacao={e.localizacao}
+                        quartos={e.quartos}
+                        mensalidade={e.mensalidade}
+                        valorEmprestimo={e.valorEmprestimo}
+                        rendimentos={e.rendimentos}
+                        ganhosMensais={e.ganhosMensais}
+                        mesesRestantes={e.mesesRestantes}
+                    />
+                ))}
+            </div>
+            <Link href="/" className="text-2xl">
+                +
+            </Link>
+        </>
+    );
 }
